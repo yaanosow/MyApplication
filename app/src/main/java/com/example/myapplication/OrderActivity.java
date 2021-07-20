f9457406c399acdd1a8af8a013c643b7b7c61103
Oleg Anosov <ya.anosow@gmail.com>
2021-07-20 19:56:09 +0300

    Добавлена кнопка навигации вверх

--- app/src/main/java/com/example/myapplication/OrderActivity.java
+++ app/src/main/java/com/example/myapplication/OrderActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class OrderActivity extends Activity {
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
