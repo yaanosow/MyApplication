de70c13cdf6735c241669a4b88a9d0a125fb3264
Oleg Anosov <ya.anosow@gmail.com>
2021-07-22 07:56:14 +0300

    добавил TopFragment, и PizzaFragment

---
+++ app/src/main/java/com/example/myapplication/PizzaFragment.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+
+import androidx.fragment.app.ListFragment;
+
+public class PizzaFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(inflater.getContext(),
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.pizzas));
+        setListAdapter(adapter);
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
